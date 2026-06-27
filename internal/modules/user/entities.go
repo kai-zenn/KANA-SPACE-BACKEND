dc026c0a64ce06464f529cd2f9eff86692961165
Kaizen <gibrantsyareza@gmail.com>
2026-06-28 00:45:35 +0700

    done configuring the user table inside entity, also creating custm DTO for any operation the use module will have

--- internal/modules/user/entities.go
+++ internal/modules/user/entities.go
@@ -1,1 +1,23 @@
 package user
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+  ID        uuid.UUID `gorm:"primary_key"`
+  FirstName string    `gorm:"not null"`
+  LastName  string    `gorm:"not null"`
+  Username  string    `gorm:"not null;unique"`
+  Email     string    `gorm:"type:varchar(100);not null;unique"`
+  PhoneNumber *string `gorm:"type:varchar(20);default:null"`
+  Password  *string    `gorm:"type:varchar(255);default:null"`
+  Address *string `gorm:"type:text;default:null"`
+  GoogleID *string    `gorm:"unique;default:null"`
+  ProfilePhotoLink string    `gorm:"default:''"`
+  Role             string    `gorm:"type:varchar(20);not null;default:'user'"`
+  CreatedAt time.Time
+	UpdatedAt time.Time
+}
--- internal/modules/user/model.go
+++ internal/modules/user/model.go
@@ -1,1 +1,62 @@
 package user
+
+import (
+  "mime/multipart"
+  "github.com/google/uuid"
+)
+
+const (
+  RoleUser    = "user"
+  RoleSeller  = "seller"
+  RoleAdmin   = "admin"
+)
+
+type UserRegisterRequest struct {
+  ID uuid.UUID `json:"-"`
+  FirstName string `json:"first_name" binding:"required"` 
+  LastName string `json:"last_name" binding:"required"` 
+  Username string `json:"username" binding:"required"`
+  PhoneNumber string `json:"phone_number" binding:"required,numeric,min=11"`
+  Email string `json:"email" binding:"required,email"`
+  Password string `json:"password" binding:"required,min=8"`
+  Role string `json:"role" binding:"required,oneof=user seller"`
+}
+
+type UserLoginRequest struct {
+  Username string `json:"username" binding:"required"`
+  Password string `json:"password" binding:"required,min=8"`
+}
+
+type GoogleAuthRequest struct {
+  IDToken string `json:"id_token" binding:"required"`
+}
+
+type UserLoginResponse struct {
+  Token string `json:"token"`
+}
+
+type UserParam struct {
+	ID       uuid.UUID `json:"-"`
+	Username string    `json:"-"`
+	Email    string    `json:"-"`
+}
+
+type UpdateProfileRequest struct {
+	FirstName        string `json:"first_name"`
+	LastName         string `json:"last_name"`
+	Username string `json:"username"`
+	PhoneNumber      string `json:"phone_number"`
+	Address          string `json:"address"`
+}
+
+type PhotoUpdate struct {
+	UserID    uuid.UUID             `json:"-"`
+	PhotoLink string                `json:"-"`
+	Image     *multipart.FileHeader `form:"image" binding:"required"`
+}
+
+type UpgradeSellerRequest struct {
+    UserID   uuid.UUID `json:"-"`
+    ShopName string    `json:"shop_name" binding:"required"`
+    Address  string    `json:"address" binding:"required"`
+}
