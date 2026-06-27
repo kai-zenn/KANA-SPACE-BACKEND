bb10f1ae3c3f97081464fd7c2dfd3f309e9d7ef9
Kaizen <gibrantsyareza@gmail.com>
2026-06-27 15:14:11 +0700

    done configuring jwt and bcrypt package, this will be global method/function that will be called for authentication on user module

---
+++ internal/pkgs/bcrypt/bcrypt.go
@@ -0,0 +1,23 @@
+package bcrypt
+
+import "golang.org/x/crypto/bcrypt"
+
+type Interface interface {
+  GenerateHashPassword(password string) (string, error)
+  CompareHashPassword(hashPassword string, password string) error
+}
+
+type cryptoBcrypt struct {}
+
+func newCryptoBcrypt() Interface {
+	return &cryptoBcrypt{}
+}
+
+func (c *cryptoBcrypt) GenerateHashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(bytes), err
+}
+
+func (c *cryptoBcrypt) CompareHashPassword(hashPassword string, password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashPassword), []byte(password))
+}
---
+++ internal/pkgs/jwt/jwt.go
@@ -0,0 +1,67 @@
+package jwtauth
+
+import (
+	"errors"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+type Claims struct {
+	UserId uuid.UUID `json:"user_id"`
+	Role   string    `json:"role"`
+	jwt.RegisteredClaims
+}
+
+type Interface interface {
+	GenerateToken(userId uuid.UUID, role string) (string, error)
+	ValidateToken(tokenString string) (*Claims, error)
+}
+
+type jsonWebToken struct {
+	SecretKey   []byte
+	ExpiredTime time.Duration
+}
+
+func NewJWTToken(secretKey string, expire time.Duration) Interface {
+  return &jsonWebToken{
+    SecretKey: []byte(secretKey),
+    ExpiredTime: expire,
+  }
+}
+
+func (j *jsonWebToken) GenerateToken(userId uuid.UUID, role string) (string, error) {
+  claims := Claims{
+    UserId: userId,
+    Role: role,
+    RegisteredClaims: jwt.RegisteredClaims{
+      ExpiresAt: jwt.NewNumericDate(time.Now().Add(j.ExpiredTime)),
+      IssuedAt: jwt.NewNumericDate(time.Now()),
+    },
+  }
+
+  token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
+  return token.SignedString(j.SecretKey)
+}
+
+func (j *jsonWebToken) ValidateToken(tokenString string) (*Claims, error) {
+  token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func (token *jwt.Token) (interface{}, error) {
+    return j.SecretKey, nil
+  })
+  
+  if token == nil {
+    return nil, errors.New("Token is nil")
+  }
+  
+  if err != nil {
+    return nil, err
+  }
+
+  claims, ok := token.Claims.(*Claims)
+  if !ok || !token.Valid {
+    return nil, errors.New("Invalid token")
+  }
+
+  return claims, nil
+}
