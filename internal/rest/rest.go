cb42d8232090b76e6a1d10fb222d3bc9b5eb9376
Kaizen <gibrantsyareza@gmail.com>
2026-07-14 22:54:17 +0700

    adding space module route, this include post, like, and comment endpoint. im stil testing on this one, because most of the data being sent does not fit as the spec already specified.

--- internal/rest/rest.go
+++ internal/rest/rest.go
@@ -2,6 +2,7 @@ package rest
 
 import (
 	"KANA-SPACE-BACKEND/internal/middlewares"
+	"KANA-SPACE-BACKEND/internal/modules/space"
 	"KANA-SPACE-BACKEND/internal/modules/user"
 	"KANA-SPACE-BACKEND/internal/pkgs/bcrypt"
 	"KANA-SPACE-BACKEND/internal/pkgs/jwt"
@@ -18,6 +19,7 @@ type Rest struct {
   bcrypt  bcrypt.Interface
   storage storage.Interface
   googleVerifier user.GoogleVerifierInterface
+  nlp space.NLPClientInterface
 }
 
 func NewRest(router *gin.Engine, 
@@ -25,7 +27,8 @@ func NewRest(router *gin.Engine,
   jwtAuth jwt.Interface,
   bcrypt bcrypt.Interface,
   storage storage.Interface,
-  googleVerifier user.GoogleVerifierInterface) *Rest {
+  googleVerifier user.GoogleVerifierInterface,
+  nlp space.NLPClientInterface) *Rest {
   return &Rest{
     router: router,
     db: db,
@@ -33,14 +36,16 @@ func NewRest(router *gin.Engine,
     bcrypt: bcrypt,
     storage: storage,
     googleVerifier: googleVerifier,
+    nlp: nlp,
   }
 }
 
 func (r *Rest) MountEndPoint() {
   r.router.Static("/uploads", "./uploads")
   
-  api := r.router.Group("/api/v1")
-  
+  api := r.router.Group("/api")
+
+  // -- User Module
   userRepo := user.NewUserRepository(r.db)
   userUseCase := user.NewUserUseCase(userRepo, r.bcrypt, r.jwtAuth, r.storage, r.googleVerifier)
   userHandler := user.NewUserHandler(userUseCase)
@@ -63,6 +68,33 @@ func (r *Rest) MountEndPoint() {
     userGroup.POST("/:id/follow", userHandler.FollowUsers)
     userGroup.POST("/:id/unfollow", userHandler.UnfollowUser)
   }
+
+  // -- Space Module
+  spacePostR := space.NewPostRepository(r.db)
+  spaceLikeR := space.NewLikeRepository(r.db)
+  spaceCommentR := space.NewCommentRepository(r.db)
+  
+  spacePostUseCase := space.NewPostUseCase(spacePostR, spaceCommentR, spaceLikeR, r.nlp, userRepo, r.storage)
+	spaceLikeUseCase := space.NewLikeUseCase(spaceLikeR, spacePostR)
+	spaceCommentUseCase := space.NewCommentUseCase(spaceCommentR, spacePostR)
+
+	spaceHandler := space.NewSpaceHandler(spacePostUseCase, spaceCommentUseCase, spaceLikeUseCase)
+
+	spaceGroup := api.Group("/space")
+	spaceGroup.Use(middlewares.Authenticate(r.jwtAuth))
+	{
+		spaceGroup.POST("/posts", spaceHandler.CreatePost)       
+		spaceGroup.GET("/posts", spaceHandler.GetFeed)          
+		// spaceGroup.GET("/posts/:id", spaceHandler.FindPostByID)  // GET /api/v1/posts/:id (Detail post)
+		spaceGroup.DELETE("/posts/:id", spaceHandler.DeletePost)
+
+		spaceGroup.POST("/posts/:id/like", spaceHandler.LikePost)     
+		spaceGroup.DELETE("/posts/:id/like", spaceHandler.UnlikePost) 
+
+		spaceGroup.POST("/posts/:id/comments", spaceHandler.CreateComment)    
+		spaceGroup.GET("/posts/:id/comments", spaceHandler.GetComments)  
+		spaceGroup.DELETE("/posts/comments/:comment_id", spaceHandler.DeleteComment) 
+	}
 } 
 
 func (r *Rest) Serve(port string) {
